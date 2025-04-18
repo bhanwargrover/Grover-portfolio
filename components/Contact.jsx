0a7b2c0f3fc87ea0aa81361cf96ac0752ccddeb4
Bhanwar Grover <bhavargrover4@gmail.com>
2025-04-18 15:08:47 +0530

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -37,7 +37,7 @@ function Contact() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://grover-portfolio.onrender.com/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
